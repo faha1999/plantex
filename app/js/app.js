a1d098a34a63361ac7cae56f91d5e8744072613a
faha1999 <kafahad1999@gmail.com>
2022-02-15 22:32:31 +0600

    faq completed

--- app/js/app.js
+++ app/js/app.js
@@ -37,3 +37,32 @@ function scrollHeader() {
   else header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
+
+/*=============== QUESTIONS ACCORDION ===============*/
+const accordionItems = document.querySelectorAll('.questions_item');
+
+accordionItems.forEach((item) => {
+  const accordionHeader = item.querySelector('.questions_header');
+
+  accordionHeader.addEventListener('click', () => {
+    const openItem = document.querySelector('.accordion-open');
+
+    toggleItem(item);
+
+    if (openItem && openItem !== item) {
+      toggleItem(openItem);
+    }
+  });
+});
+
+const toggleItem = (item) => {
+  const accordionContent = item.querySelector('.questions_content');
+
+  if (item.classList.contains('accordion-open')) {
+    accordionContent.removeAttribute('style');
+    item.classList.remove('accordion-open');
+  } else {
+    accordionContent.style.height = accordionContent.scrollHeight + 'px';
+    item.classList.add('accordion-open');
+  }
+};
